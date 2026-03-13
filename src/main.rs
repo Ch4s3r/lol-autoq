200092cb97fc769cf532b8e2fe04be02add92ba4
Ch4s3r <patrick.lampe.dittmer@gmail.com>
2026-03-13 19:30:00 +0100

    feat(logging): initialize tracing subscriber for enhanced logging capabilities

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,13 @@ mod lcu;
 mod ui;
 
 fn main() {
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| tracing_subscriber::EnvFilter::new("info")),
+        )
+        .init();
+
     let head = format!("<style>{}</style>", ui::styles::CSS);
 
     dioxus::LaunchBuilder::desktop()
